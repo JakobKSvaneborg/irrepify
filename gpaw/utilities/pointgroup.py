9193f6499534eff1c57e867e0abdc830658923e1
Kartikeya Sharma <kartsh@dtu.dk>
2025-11-28 12:52:10 +0100

    added more pgs

--- gpaw/utilities/pointgroup.py
+++ gpaw/utilities/pointgroup.py
@@ -120,7 +120,38 @@ class SPGOperations:
         w_sc = dataset["translations"]
         origin_shift_c = dataset["origin_shift"]
         cell_cv = np.array(atoms.cell)
-        dct = {"-6m2": "D3h", "3m": "C3v", "mm2": "C2v"}
+        dct = {"1": "C1",
+               "-1": "Ci",
+               "2": "C2",
+               "m": "Cs",
+               "2/m": "C2h",
+               "222": "D2",
+               "mm2": "C2v",
+               "mmm": "D2h",
+               "3": "C3",
+               "-3": "S6",
+               "32": "D3",
+               "3m": "C3v",
+               "-3m": "D3d",
+               "4": "C4",
+               "-4": "S4",
+               "4/m": "C4h",
+               "422": "D4",
+               "4mm": "C4v",
+               "-32m": "D2d",
+               "4/mmm": "D4h",
+               "6": "C6",
+               "-6": "C3h",
+               "6/m": "C6h",
+               "622": "D6",
+               "6mm": "C6v",
+               "-6m2": "D3h",
+               "6/mmm": "D6h",
+               "23": "T",
+               "m-3": "Th",
+               "432": "O",
+               "-43m": "Td",
+               "m-3m": "Oh"}
         pointgroup = dct[dataset["pointgroup"]]
         return cls(W_scc, w_sc, origin_shift_c, cell_cv, pointgroup)
 
